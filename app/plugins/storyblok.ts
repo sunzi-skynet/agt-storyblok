76c138a7997bc0889cc516709acff4ac5a7ec0e3
Sunzi <sunzi.skynet@gmail.com>
2026-02-03 00:31:12 +0000

    fix: hero_teaser Komponente im Plugin registrieren

--- app/plugins/storyblok.ts
+++ app/plugins/storyblok.ts
@@ -4,6 +4,7 @@ import Teaser from '../storyblok/Teaser.vue'
 import Grid from '../storyblok/Grid.vue'
 import Feature from '../storyblok/Feature.vue'
 import Hero from '../storyblok/Hero.vue'
+import HeroTeaser from '../storyblok/hero_teaser.vue'
 import Testimonial from '../storyblok/Testimonial.vue'
 import TextSection from '../storyblok/TextSection.vue'
 
@@ -13,6 +14,7 @@ export default defineNuxtPlugin(({ vueApp }) => {
   // Register components with lowercase names to match Storyblok
   vueApp.component('page', Page)
   vueApp.component('teaser', Teaser)
+  vueApp.component('hero_teaser', HeroTeaser)
   vueApp.component('grid', Grid)
   vueApp.component('feature', Feature)
   vueApp.component('hero', Hero)
