59711fd0fad7d1a40982dfa5b8a716c0e947a5f1
Sunzi <sunzi.skynet@gmail.com>
2026-02-03 13:42:38 +0000

    chore: remove incomplete components (booking_form, footer_section, text_section + nested)

---
+++ app/plugins/storyblok-components.ts
@@ -0,0 +1,49 @@
+// Manual component registration
+// NOTE: @storyblok/vue converts snake_case to kebab-case internally
+// So booking_form becomes booking-form when resolving
+
+// Base components
+import Page from '../storyblok/Page.vue'
+import Teaser from '../storyblok/Teaser.vue'
+import Grid from '../storyblok/Grid.vue'
+import Feature from '../storyblok/Feature.vue'
+import Hero from '../storyblok/Hero.vue'
+import HeroTeaser from '../storyblok/HeroTeaser.vue'
+import Testimonial from '../storyblok/Testimonial.vue'
+
+// Layout components
+import HeaderNav from '../storyblok/HeaderNav.vue'
+import NavItem from '../storyblok/NavItem.vue'
+
+// Content blocks
+import ImageTextSection from '../storyblok/ImageTextSection.vue'
+import CtaBanner from '../storyblok/CtaBanner.vue'
+import TestimonialSection from '../storyblok/TestimonialSection.vue'
+import TestimonialCard from '../storyblok/TestimonialCard.vue'
+import ClientLogos from '../storyblok/ClientLogos.vue'
+import Accordion from '../storyblok/Accordion.vue'
+import AccordionItem from '../storyblok/AccordionItem.vue'
+
+export default defineNuxtPlugin(({ vueApp }) => {
+  // Register with KEBAB-CASE names (Storyblok converts _ to - internally)
+  vueApp.component('page', Page)
+  vueApp.component('teaser', Teaser)
+  vueApp.component('hero-teaser', HeroTeaser)
+  vueApp.component('grid', Grid)
+  vueApp.component('feature', Feature)
+  vueApp.component('hero', Hero)
+  vueApp.component('testimonial', Testimonial)
+  
+  // Layout
+  vueApp.component('header-nav', HeaderNav)
+  vueApp.component('nav-item', NavItem)
+  
+  // Content blocks
+  vueApp.component('image-text-section', ImageTextSection)
+  vueApp.component('cta-banner', CtaBanner)
+  vueApp.component('testimonial-section', TestimonialSection)
+  vueApp.component('testimonial-card', TestimonialCard)
+  vueApp.component('client-logos', ClientLogos)
+  vueApp.component('accordion', Accordion)
+  vueApp.component('accordion-item', AccordionItem)
+})
